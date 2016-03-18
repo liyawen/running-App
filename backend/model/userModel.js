5acc73d5c8b73acd7f339f4d533a3d45bb2cd3d0
liyawen <413235793@qq.com>
2016-03-18 23:39:13 +0800

    test

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -7,55 +7,54 @@ function UserModel () {
 }
 
 UserModel.prototype.validate = function (req) {
-	let userMsg = req.body.userMsg;
-	let currentTid = req.body.currentTid;
+  let userMsg = req.body.userMsg;
+  let currentTid = req.body.currentTid;
 
-	if (!currentTid) {
-		return new Error('tid为空！');
-	}
-	 return false;
+  if (!currentTid) {
+    return new Error('tid为空！');
+  }
+   return false;
 }
 
 UserModel.prototype.register = function (req) {
-	let me = this;
-	let userMsg = req.body.userMsg;
-	let currentTid = req.body.currentTid;
+  let me = this;
+  let userMsg = req.body.userMsg;
+  let currentTid = req.body.currentTid;
 
-	let error = me.validate(req);
-	if (error) {
-		return Promise.resolve({
-			status: 1,
-			msg: error.message
-		});
-	}
+  let error = me.validate(req);
+  if (error) {
+    return Promise.resolve({
+      status: 1,
+      msg: error.message
+    });
+  }
+  let email = '';
 
-	return db.query(`select * from user_tmp where id = '${currentTid}'`).then(function (res) {
-		if (res.length <= 0) {
-			return new Error('不存在该用户！');
-		}
-		console.log(res);
-		return res;
-	}).then(function (res) {
-		let sql = `insert into user (email, password, nickname, gendle, age, 
-			height, weight, shoeSize, pelvisHeight, pelvisWidth, KneeHeight, KneeWidth, 
-			AnkleHeight, AnkleWidth, salt) VALUES ('${res[0].email}','${res[0].password}','${userMsg[0].value}',
-			'${userMsg[1].value}','${userMsg[2].value}','${userMsg[3].value}','${userMsg[4].value}','${userMsg[5].value}','${userMsg[6].value}',
-			'${userMsg[7].value}','${userMsg[8].value}','${userMsg[9].value}','${userMsg[10].value}','${userMsg[11].value}','${res[0].salt}')`;
-		return db.query(sql);
-	}).then(function (res) {
-		console.log(res);
-		return {
-			status: 0,
-			id: res[0].id,
-			msg: '插入成功'
-		};
-	}).catch(function (err) {
-		return {
-			status: 1,
-			msg: err.message,
-			sql: err.sql
-		};
-	});
+  return db.query(`select * from user_tmp where id = '${currentTid}'`).then(function (res) {
+    if (res.length <= 0) {
+      return new Error('不存在该用户！');
+    } else {
+      email = res[0].email;
+      let sql = `insert into user (email, password, nickname, gendle, age, 
+        height, weight, shoeSize, pelvisHeight, pelvisWidth, KneeHeight, KneeWidth, 
+        AnkleHeight, AnkleWidth, salt) VALUES ('${res[0].email}','${res[0].password}','${userMsg[0].value}',
+        '${userMsg[1].value}','${userMsg[2].value}','${userMsg[3].value}','${userMsg[4].value}','${userMsg[5].value}','${userMsg[6].value}',
+        '${userMsg[7].value}','${userMsg[8].value}','${userMsg[9].value}','${userMsg[10].value}','${userMsg[11].value}','${res[0].salt}')`;
+      return db.query(sql);
+    }
+  }).then(function (res) {
+    return {
+      status: 0,
+      email: email,
+      msg: '插入成功'
+    };
+  }).catch(function (err) {
+    return {
+      status: 1,
+      msg: err.message,
+      sql: err.sql
+    };
+  });
 }
 
 module.exports = UserModel;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,7 @@ let router = express.Router();
 let UserTmpModel = require('../model/userTmpModel');
 let UserModel = require('../model/userModel');
 let UserLoginModel = require('../model/userLoginModel');
+let RecordModel = require('../model/recordModel');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -31,6 +32,14 @@ router.post('/login/', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   let model = new UserLoginModel();
   model.login(req).then(function (result) {
+    res.send(JSON.stringify(result));
+  });
+});
+
+router.get('/getRunRecords/', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  let model = new RecordModel();
+  model.getRunRecords(req).then(function (result) {
     res.send(JSON.stringify(result));
   });
 });
