a447380282c086b259bdc5eaa9aebc0a3a842d88
yedeying999 <yedeying999@gmail.com>
2016-03-21 01:57:05 +0800

    save

--- frontend/www/js/controllers/recordCtrl.js
+++ frontend/www/js/controllers/recordCtrl.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers')
 
 .controller('recordCtrl', function ($scope, $state, $stateParams, $rootScope, Record) {
-  let rid = parseInt($stateParams.rid);
+  var rid = parseInt($stateParams.rid);
   Record.getDetail(rid, function (detail) {
     $scope.detail = detail;
     $rootScope.currentDetail = detail;
