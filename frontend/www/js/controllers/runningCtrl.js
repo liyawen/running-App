202240c6e11f74cf7c78074aa1de64bc571d8b55
yedeying999 <yedeying999@gmail.com>
2016-05-07 12:14:01 +0800

    save

--- frontend/www/js/controllers/runningCtrl.js
+++ frontend/www/js/controllers/runningCtrl.js
@@ -2,5 +2,36 @@ angular.module('starter.controllers')
 
 .controller('runningCtrl', function ($scope, $state, $stateParams, Running, back) {
   $scope.goback = back;
-  
+  var initialState = {
+    averageCadence: '0',
+    averagePace: '0',
+    distance: '0',
+    runTime: '0',
+    fatigueIndex: '--',
+    fatigueIndexLast: '--',
+    healthIndex: '--'
+  };
+  var currentState = false;
+  var interval = null;
+  $scope.data = initialState;
+  $scope.actionName = 'Start';
+
+  var updateData = function () {
+    $scope.data = Running.getRandomData($scope.data);
+  }
+
+  $scope.toggle = function () {
+    currentState = !currentState;
+    if (currentState) {
+      interval = setInterval(function () {
+        $scope.$apply(updateData);
+      }, 500);
+      $scope.actionName = 'Stop';
+    } else {
+      if (interval) {
+        clearInterval(interval);
+      }
+      $scope.actionName = 'Start';
+    }
+  };
 });
--- frontend/www/js/services/page/runningService.js
+++ frontend/www/js/services/page/runningService.js
@@ -1,5 +1,92 @@
+function random(left, right, isInt) {
+    let size = right - left;
+    let ret = Math.random() * size + left;
+    if (isInt) {
+        ret = parseInt(ret, 10);
+    }
+    return ret;
+}
+var fields = {
+  distance: [0.001, 0.002],
+  averagePace: [5, 10],
+  averageCadence: [0.5, 3],
+  fatigueIndex: [0.6, 1.0],
+  healthIndex: [0.6, 1.0],
+  landingStrike: [0, 3, true],
+  landingPronation: [0, 3, true],
+  landingPronationAngle: [-20, 20],
+  impactPower: [100, 250],
+  strideLength: [100, 170],
+  xCOM: [0, 10],
+  verticalMagnitude: [15, 20],
+  energyUtilization: [0, 1],
+  runTime: [0.005, 0.0083],
+  calorie: [0, 50]
+};
+var map1 = ['frontHeelStrike','midFootStrike','backHeelStrike'];
+var map2 = ['normalPronation','overPronation','underpronation'];
+var curState = null;
+var curRecordId = null;
+var
+
 angular.module('starter.services')
 
 .factory('Running', function ($http, $state, buildUrl, round) {
-  return true;
+  return {
+    getRecordId: function () {
+      return curRecordId;
+    },
+
+    initial: function (callback) {
+      var curState = {
+        averageCadence: 0,
+        aveargePace: 0,
+        distance: 0,
+        runTime: 0,
+        fatigueIndex: 1,
+        fatigueIndexLast: 1,
+        healthIndex: 1
+      };
+      $http.get(buildUrl('/newRecord')).success(function (res) {
+        curRecordId = res.id;
+        callback();
+      });
+    },
+
+    getRandomData: function () {
+      var row = {
+        datatime: +(new Date())
+      };
+      Object.keys(fields).forEach(function (field) {
+        row[field] = random.apply(null, fields[field]);
+      });
+      row.landingStrike = map1[row.landingStrike];
+      row.landingPronation = map2[row.landingPronation];
+      this.uploadData(row);
+
+      var initialState = {
+        averageCadence: row.averageCadence,
+        averagePace: row.averagePace,
+        distance: curState.distance + row.distance,
+        runTime: curState.runTime + row.runTime,
+        fatigueIndex: row.fatigueIndex,
+        fatigueIndexLast: '--',
+        healthIndex: row.healthIndex
+      };
+      curState = JSON.parse(JSON.stringify(initialState));
+
+      ['averageCadence', 'averagePace', 'distance', 'runTime'].forEach(function (field) {
+        initialState[field] = round(initialState[field], 2);
+      });
+      ['fatigueIndex', 'fatigueIndexLast', 'healthIndex'].forEach(function (field) {
+        initialState[field] = round(initialState[field], 2, '%');
+      });
+
+      return initialState;
+    },
+
+    uploadData: function (data) {
+      $http.post(buildUrl('/uploadData'), data);
+    }
+  };
 })
